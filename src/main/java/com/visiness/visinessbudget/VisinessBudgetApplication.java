e05af0086fbf7f7d46b60a270756b43f1f396afc
Alexander Ramirez <aramirez@teravisiontech.com>
2022-07-26 22:02:54 -0400

    Initial commit

---
+++ src/main/java/com/visiness/visinessbudget/VisinessBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.visiness.visinessbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisinessBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisinessBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visiness/visinessbudget/VisinessBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visiness.visinessbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisinessBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
